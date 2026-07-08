171286b89c75db805410795f4b2a60a39aad693c
sarthakz-47 <sarthakz.4100@gmail.com>
2026-07-09 00:58:00 +0530

    User Model & Controllers

--- server/index.js
+++ server/index.js
@@ -1,12 +1,26 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 import connectDB from "./database/dbConnect.js";
+import userRoute from "./routes/user.route.js";
 
-dotenv.config();
+dotenv.config({});
 connectDB();
 const app = express();
-const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
+
+app.use("/api/v1/user", userRoute);
+
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server listening at port ${PORT}`);
+  console.log(`Server listen at port ${PORT}`);
 });
