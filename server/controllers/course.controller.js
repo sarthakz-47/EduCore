248cb1e905de90d838b5805144966c406a468cbf
sarthakz-47 <sarthakz.4100@gmail.com>
2026-07-10 16:28:11 +0530

    minor bugs ignore

--- server/controllers/course.controller.js
+++ server/controllers/course.controller.js
@@ -2,15 +2,15 @@ import { Course } from "../models/course.model.js";
 
 export const createCourse = async (req, res) => {
   try {
-    const { courseTittle, category } = req.body;
-    if (!courseTittle || !category) {
+    const { courseTitle, category } = req.body;
+    if (!courseTitle || !category) {
       return res.status(400).json({
         message: "Course title and category are required",
       });
     }
 
     const course = await Course.create({
-      courseTittle,
+      courseTitle,
       category,
       creator: req.id,
     });
