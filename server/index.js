88770b623b37a0272418db586aeee6e6e85cc2fe
sarthakz-47 <sarthakz.4100@gmail.com>
2026-07-15 19:09:38 +0530

    Build Course Progress Page

--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ import userRoute from "./routes/user.route.js";
 import courseRoute from "./routes/course.route.js";
 import mediaRoute from "./routes/media.route.js";
 import purchaseRoute from "./routes/purchaseCourse.route.js";
+import courseProgressRoute from "./routes/courseProgress.route.js";
 
 dotenv.config({});
 connectDB();
@@ -25,6 +26,8 @@ app.use("/api/v1/media", mediaRoute);
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/course", courseRoute);
 app.use("/api/v1/purchase", purchaseRoute);
+app.use("/api/v1/progress", courseProgressRoute);
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server listen at port ${PORT}`);
